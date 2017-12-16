48f02ee50d7ae92c146258ee4c8e2ae5a16a2eed
Chip Bell <chipbell4@gmail.com>
2017-12-15 21:58:46 -0500

    Glorious racket

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+'use strict';
+
 const Speaker = require('speaker');
+const Readable = require('stream').Readable;
+
+let stream = new Readable();
+let t = 0.0
+stream._read = function() {
+  for(let k = 0; k < 2000; k++) {
+    let amplitude = (Math.sin(t) + 1) * 0.5 * 65535;
+    let letter = String.fromCharCode(Math.floor(amplitude));
+    stream.push(letter);
+    t += 0.1;
+  }
+};
 
 // Create the Speaker instance
 const speaker = new Speaker({
-  channels: 2,          // 2 channels
-  bitDepth: 16,         // 16-bit samples
-  sampleRate: 44100     // 44,100 Hz sample rate
+  channels: 1,
+  bitDepth: 16,
+  sampleRate: 20000
 });
 
 // PCM data from stdin gets piped into the speaker
-process.stdin.pipe(speaker);
+stream.pipe(speaker);
