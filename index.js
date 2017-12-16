2a9b4bbec595530b0b32dc0f26809d609b047291
Chip Bell <chipbell4@gmail.com>
2017-12-15 23:43:49 -0500

    Terminal UI hype

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
 'use strict';
 
 const audio = require('./audio');
+const blessed = require('blessed');
+
+let screen = blessed.screen({
+  smartCSR: true
+});
+
+screen.title = 'Synth Yo'
+
+// Create a box perfectly centered horizontally and vertically.
+var box = blessed.box({
+  top: 'center',
+  left: 'center',
+  width: '50%',
+  height: '50%',
+  content: 'Hello {bold}world{/bold}!',
+  tags: true,
+  border: {
+    type: 'line'
+  },
+  style: {
+    fg: 'white',
+    bg: 'magenta',
+    border: {
+      fg: '#f0f0f0'
+    },
+    hover: {
+      bg: 'green'
+    }
+  }
+});
+
+// Append our box to the screen.
+screen.append(box);
+
+// If box is focused, handle `enter`/`return` and give us some more content.
+box.key('enter', function(ch, key) {
+  box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
+  box.setLine(1, 'bar');
+  box.insertLine(1, 'foo');
+  screen.render();
+});
+
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
+
+// Focus our element.
+box.focus();
+screen.render();
