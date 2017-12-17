0cf3b2d1504a327063fa4c79f9c063d66d838308
Chip Bell <chipbell4@gmail.com>
2017-12-17 13:42:38 -0500

    Oscillator node stub out

--- audio.js
+++ audio.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const NoiseNode = require('./nodes/NoiseNode');
+const OscillatorNode = require('./nodes/OscillatorNode');
 const SpeakerNode = require('./nodes/SpeakerNode');
 
 let noise = new NoiseNode();
+let oscillator = new OscillatorNode(440, 'sine');
 let speaker = new SpeakerNode();
 
-speaker.receive('speaker', noise);
+speaker.receive('speaker', oscillator);
---
+++ nodes/OscillatorNode.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Node = require('./Node');
+const constants = require('../constants');
+
+class OscillatorNode extends Node {
+  constructor(frequency, waveform) {
+    super();
+
+    this.frequency = frequency || 440;
+    this.waveform = waveform || 'sine';
+
+    let dt = 1 / constants.SAMPLE_RATE;
+    this.outputStream._read = function() {
+      let period = 1 / this.frequency; 
+      for(let t = 0; t < period; t += dt) {
+        let rawValue = 0;
+        if(this.waveform === 'sine') {
+          rawValue = this.sine(t);
+        }
+        if(this.waveform === 'square') {
+          rawValue = this.square(t);
+        }
+        this.outputStream.push(this.rawValueToByte(rawValue));
+      }
+    }.bind(this);
+  }
+
+  sine(t) {
+    return Math.sin(t * this.frequency * 2 * Math.PI);
+  }
+
+  rawValueToByte(rawValue) {
+    var value = Math.floor((rawValue + 1) * 64);
+
+    return String.fromCharCode(value);
+  }
+}
+
+module.exports = OscillatorNode;
