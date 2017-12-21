82d9cab8670068a977df0ef3ca97a73fc7ad28f1
Chip Bell <chipbell4@gmail.com>
2017-12-20 19:57:58 -0500

    Crazy sequencer stuff

--- audio.js
+++ audio.js
@@ -5,20 +5,25 @@ const OscillatorNode = require('./nodes/OscillatorNode');
 const LowPassFilterNode = require('./nodes/LowPassFilterNode');
 const SpeakerNode = require('./nodes/SpeakerNode');
 const MidiNode = require('./nodes/MidiNode');
+const SequencerNode = require('./nodes/SequencerNode');
 
 let noise = new NoiseNode();
 let oscillator = new OscillatorNode();
 let lfo = new OscillatorNode();
 let lpf = new LowPassFilterNode();
-let midi = new MidiNode();
+//let midi = new MidiNode();
+let sequencer = new SequencerNode();
 let speaker = new SpeakerNode();
 
 lfo.waveform = 'sine';
-lfo.amplitude = () => 0.1;
-lfo.carrier = () => 10;
+lfo.amplitude = () => 1;
+lfo.carrier = () => 0.5;
+
+sequencer.sequence = [110, 110, 220, 220, 150, 150, 110, 150];
+sequencer.length = lfo.value;
 
 oscillator.waveform = 'square';
-oscillator.carrier = midi.value;
+oscillator.carrier = sequencer.value;
 
 let lfo2 = new OscillatorNode();
 lfo2.amplitude = () => 100;
---
+++ nodes/SequencerNode.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Node = require('./Node');
+
+class SequencerNode extends Node {
+  constructor() {
+    super();
+
+    this.sequence = [440];
+    this.length = () => 0.5;
+  }
+
+  value(t) {
+    var index = Math.floor(t / this.length(t)) % this.sequence.length;
+    return this.sequence[index];
+  };
+}
+
+module.exports = SequencerNode;
