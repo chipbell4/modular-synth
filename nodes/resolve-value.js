0f49b7159a16560bfceca78c3dde642da09f4737
Chip Bell <chipbell4@gmail.com>
2017-12-24 14:07:44 -0500

    Example sequencer usage

--- nodes/resolve-value.js
+++ nodes/resolve-value.js
@@ -6,6 +6,10 @@ module.exports = function(value) {
   if(typeof value === 'string') {
     return value;
   }
+  
+  if(value instanceof Array) {
+    return value;
+  }
 
   if(typeof value === 'number') {
     return () => value;
