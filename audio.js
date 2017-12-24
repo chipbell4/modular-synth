f0c1f9d481f42126f6a3f8e327a11defc93dd94b
Chip Bell <chipbell4@gmail.com>
2017-12-24 08:24:46 -0500

    Setting up to be configurable

--- audio.js
+++ audio.js
@@ -7,29 +7,14 @@ const SpeakerNode = require('./nodes/SpeakerNode');
 const MidiNode = require('./nodes/MidiNode');
 const SequencerNode = require('./nodes/SequencerNode');
 
-let noise = new NoiseNode();
-let oscillator = new OscillatorNode();
-let lfo = new OscillatorNode();
-let lpf = new LowPassFilterNode();
-//let midi = new MidiNode();
-let sequencer = new SequencerNode();
-let speaker = new SpeakerNode();
+const Synth = {
+  noise: new NoiseNode(),
+  lfo1: new OscillatorNode(),
+  lfo2: new OscillatorNode(),
+  sequencer1: new SequencerNode(),
+  sequencer2: new SequencerNode(),
+  lpf: new LowPassFilterNode(),
+  speaker: new SpeakerNode()
+};
 
-lfo.waveform = 'sine';
-lfo.amplitude = () => 1;
-lfo.carrier = () => 0.5;
-
-sequencer.sequence = [110, 110, 220, 220, 150, 150, 110, 150];
-sequencer.length = lfo.value;
-
-oscillator.waveform = 'square';
-oscillator.carrier = sequencer.value;
-
-let lfo2 = new OscillatorNode();
-lfo2.amplitude = () => 100;
-lfo2.carrier = () => 8;
-
-lpf.filterWidth = lfo2.value;
-lpf.input = oscillator.value;
-
-speaker.input = lpf.value;
+module.exports = Synth;
